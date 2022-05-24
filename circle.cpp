8d728229ce56cd0518751e7d613779373ab29a35
UseresuK <99245652+UseresuK@users.noreply.github.com>
2022-05-24 16:11:36 +0300

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,39 @@
+#include "circle.hpp"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+Circle::Circle() : radius(0.0) { counter++; }
+Circle::Circle(float radius) {
+    this->radius = radius;
+    counter++;
+}
+Circle::Circle(const Circle& copy_circle) {
+    radius = copy_circle.radius;
+
+}
+Circle::~Circle() {
+    counter--;
+    delete this;
+}
+
+float Circle::area() { return 3.14 * radius * radius; }
+
+void Circle::show() {
+    cout << "Circle:\n Radius = " << radius << "\n Area = " << this->area() << endl;
+}
+
+void Circle::edit() {
+    float x;
+    cout << " Enter radius " << endl;
+    cin >> x;
+    this->radius = x;
+    cout << "Editing is over. Radius = " << radius << endl;
+}
+int Circle::getid() { return 2; }
+
+void Circle::fprint(ofstream& path) {
+    path << this->radius << " ";
+}
+
